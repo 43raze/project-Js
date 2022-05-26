600becd508a4a26020e11ed7a945fa348db5c540
Serhii Hul <owna.sgul@gmail.com>
2022-05-26 12:04:24 +0300

    base learn JS

---
+++ app.js
@@ -0,0 +1,63 @@
+'use strict'
+//let number = 5;
+
+//const leftBorderWidth = 1;
+//number = 10;
+//console.log(number);
+
+//const obj = {
+//	a: 50
+//};
+
+//obj.a = 10;
+//console.log(obj);
+
+//let number2 = 4.6;
+//console.log(-4 / 0); // !infinity
+//console.log('string' * 9); // ! NaN
+
+//const persone = `5`; //! будет строка
+
+//let bool = true; //! false-нет
+
+//const obj2 = {
+//	name: 'John',
+//	age: 25,
+//	isMarried: false
+//};
+//console.log(obj2['isMarried']); // ! или точка "."
+
+//let arr = ['plump.png', 'orange.jpg', 6, 'apple.bmp', {},
+//	[]
+//]
+//console.log(arr[1]);
+/***********************/
+
+//alert('Hello world!');
+
+//const result =confirm('Are you there?');
+
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?', '');
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя');
+//answers[1] = prompt('Как ваше фамилия');
+//answers[2] = prompt('Как вам лет');
+
+//console.log(typeof (answers));
+//console.log(typeof (null));
+
+/***********************/
+
+//! ИНТЕРПОЛЯЦИЯ
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`); // ! + category старый вариант!
+
+const user = 'Ivan';
+
+console.log(`Привет, ${user}`);
