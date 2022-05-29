b8ed6420d600bfaba1e971dad335189f12b32626
Serhii Hul <owna.sgul@gmail.com>
2022-05-29 17:25:29 +0300

    aa

--- js/script.js
+++ js/script.js
@@ -129,3 +129,139 @@ function learnJS(lang, callback) {
 learnJS('JavaScript', function () {
 	console.log('я прошел этот урок');
 });
+
+const arr = [2, 3, 6, 8, 12];
+arr.forEach(function (item, i, arr) {
+	console.log(`${i}: ${item} внутри массива ${arr}`);
+}); // ! Где i это номер массива, item это количество в массиве, третий аргумент это ссылка на элемент в массиве!!!!
+
+for (let value of arr) {
+	console.log(value);
+}
+
+const str2 = prompt('', '');
+const products = str2.split(',');
+products.sort();
+console.log(products.join(';'));
+
+function copy(mainOdj) {
+	let objCopy = {};
+
+	let key;
+	for (key in mainOdj) {
+		objCopy[key] = mainOdj[key];
+	}
+	return objCopy;
+}
+
+//! Оператор разворота!!!
+const video = ['youtube', 'vimio', 'rytube'];
+const blogs = ['worpress', 'livejournal', 'bloger'];
+const internet = [...video, ...blogs, 'vk', 'faceboock'];
+console.log(internet);
+
+const array = ['1', 'b'];
+const newAAray = [...array];
+
+const q = {
+	one: 1,
+	two: 2
+};
+
+const newObj = {
+	...q
+};
+
+//! ООП основы
+
+let str3 = 'some';
+let strObj = new String(str3);
+
+console.log(typeof (str3));
+console.log(typeof (strObj));
+
+console.dir([1, 2, 3]);
+
+
+// const box = document.getElementById('box');
+// console.log(box);
+
+// const buttos = document.getElementsByTagName('button');
+
+// console.log(buttos[1]); //! выбрать элемен из колекции []
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+
+// hearts.forEach(item => {
+// 	console.log(item);
+// });
+
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
+//! practice
+const
+	box = document.getElementById('box'),
+	buttos = document.getElementsByTagName('button'),
+	circles = document.getElementsByClassName('circle'),
+	hearts = document.querySelectorAll('.heart'),
+	oneHeart = document.querySelector('.heart'),
+	wrapper = document.querySelector('.wrapper');
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+
+box.style.cssText = 'background-color: blue; width: 100px;';
+
+buttos[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+// hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+	item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+wrapper.append(div); //! append добавить элемент  в конец
+//wrapper.prepend(div);  //! append добавить элемент  в начало
+
+//hearts[1].before(div);
+//hearts[1].after(div);
+
+// circles[0].remove();
+//wrapper.removeChild(hearts[1]); //! старый синтаксис
+
+// hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[1]);
+
+// div.innerHTML = '<h1>Hello World</h1>';
+
+div.insertAdjacentHTML('', '<h2>hello</h2>');
+// div.textContent= 'hello';
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', (e) => {
+	e.target.remove();
+});
+
+let i = 0;
+const deleteElement = (e) => {
+	console.log(e.target);
+	i++;
+	if (i == 1) {
+		btn.removeEventListener('click', deleteElement);
+	}
+};
+
+btn.addEventListener('click', deleteElement);
